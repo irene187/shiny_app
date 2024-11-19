a83ec2319ba9c5e715447ac26ccb12b7842ce1f6
Sandra <sandra.aguilera@institutmetropoli.cat>
2024-11-19 11:59:48 +0100

    Update estadistics_descriptius.R
    He canviat l'ordre dels estadístics i he afegit el coeficient de variació (100*dt/mitjana).

--- estadistics_descriptius.R
+++ estadistics_descriptius.R
@@ -9,18 +9,19 @@ taula_estad <- function(var_num){
   
   # Calcular els estadístics
   estadistics <- data.frame(
-    estadistic = c("Mínim", "1r Quartil", "Mediana", "Mitjana", "3r Quartil", "Màxim", "Desviació Estàndard", "Variança", "N (nombre de dades)", "Nas"),
+    estadistic = c("Mitjana", "Variança", "dt", "Q1", "Q2", "Q3", "Màxim", "Mínim", "N", "NA", "Coeficient variació"),
     valor = c(
-      min(var_num, na.rm = TRUE),
-      quantile(var_num, 0.25, na.rm = TRUE),
-      median(var_num, na.rm = TRUE),
       mean(var_num, na.rm = TRUE),
+      var(var_num, na.rm = TRUE),
+      sd(var_num, na.rm = TRUE),
+      quantile(var_num, 0.25, na.rm = TRUE),
+      quantile(var_num, 0.50, na.rm = TRUE),
       quantile(var_num, 0.75, na.rm = TRUE),
       max(var_num, na.rm = TRUE),
-      sd(var_num, na.rm = TRUE),
-      var(var_num, na.rm = TRUE),
+      min(var_num, na.rm = TRUE),
       sum(!is.na(var_num)),
-      sum(is.na(var_num))
+      sum(is.na(var_num)),
+      (100 * sd(var_num, na.rm = TRUE)/mean(var_num, na.rm = TRUE)) 
     )
   )
   
